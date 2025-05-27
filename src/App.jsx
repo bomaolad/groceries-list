0da21f2117aca8fa66a7ab90a08a8a2e01ecc3f5
dCodeBeast <bomaolad@gmail.com>
2025-05-26 20:07:03 -0400

    create starting files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header title={"Grocery List"} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
